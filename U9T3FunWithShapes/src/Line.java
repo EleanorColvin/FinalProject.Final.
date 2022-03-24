9d6579423be516c994f91ad1c5c71e2d3b76f238
BT_1E10_09 <dariusj0714@gmail.com>
2022-03-24 11:01:04 -0400

    Classes, methods added

---
+++ U9T3FunWithShapes/src/Line.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+
+public class Line extends Shape{
+
+    public Line()
+    {super();}
+
+    @Override
+    public void draw(Graphics g)
+    {
+        // set the color
+        g.setColor(getColor());
+
+        // draw the rectangle given the top left point and width and height
+        g.drawLine(getP1().x, getP1().y, getP2().x, getP2().y); // the getter methods here are INHERITED FROM SHAPE!
+    }
+}
